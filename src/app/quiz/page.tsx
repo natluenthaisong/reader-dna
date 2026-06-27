c16dde51f0bb2987765431c5cc04bb56cad88237
Nattavut Luenthaisong <nattavut.luenthaisong@gmail.com>
2026-06-28 01:37:28 +0700

    Move sticker decorations to top layer

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -117,7 +117,7 @@ const StickerDecorations = ({ questionIndex }: { questionIndex: number }) => {
   }
 
   return (
-    <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', pointerEvents: 'none', zIndex: -1 }}>
+    <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', pointerEvents: 'none', zIndex: 50 }}>
       {selectedStickers.map(s => (
         <svg 
           key={s.key} 
